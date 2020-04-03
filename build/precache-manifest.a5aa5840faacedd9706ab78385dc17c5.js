1196e4efdeef4d2f6e887c3a1f33b4fa4c4ae54a
Peter Toth <peter.toth@abehr.com>
2020-04-03 19:40:12 +0000

    build directory added to vcs

---
+++ build/precache-manifest.a5aa5840faacedd9706ab78385dc17c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "969e6beb76256860d6feeac982609e42",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8c6a9a4d0c71459f0afc",
+    "url": "./static/css/main.2a588480.chunk.css"
+  },
+  {
+    "revision": "894daed5cac13c915747",
+    "url": "./static/js/2.ee16ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee16ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6a9a4d0c71459f0afc",
+    "url": "./static/js/main.d3108a22.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
