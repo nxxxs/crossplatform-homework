53ad7ae2fdb4d15458ec9d1c55798de89c9ac265
Peter Toth <peter.toth@abehr.com>
2020-04-12 13:47:51 +0000

    build files added

---
+++ build/precache-manifest.e0cdbee15bc72f04690bc424b0a49e61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1ba2031ed0bf89360222536cb55a1d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "047f908c9084f1abe723",
+    "url": "./static/css/main.2a588480.chunk.css"
+  },
+  {
+    "revision": "894daed5cac13c915747",
+    "url": "./static/js/2.ee16ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee16ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047f908c9084f1abe723",
+    "url": "./static/js/main.3c144c53.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
